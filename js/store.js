4cc00a7a04c511dcec3660c4e17d821e6905be9b
Cristian-229 <rodriguezcristian26@hotmail.com>
2024-06-11 00:43:48 -0300

    segundo commit

--- js/store.js
+++ js/store.js
@@ -1,32 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let botonesAgregar = document.querySelectorAll('.producto__boton');
     let totalElement = document.getElementById('total');
     let total = 0;
 
-    // Recorrer los botones y agregar el evento de clic a cada uno
+
     botonesAgregar.forEach(boton => {
-        boton.addEventListener('click', function() {
-            // Acceder al elemento de unidades correspondiente al botón clickeado
+        boton.addEventListener('click', function () {
+
             let unidadesElemento = boton.parentElement.querySelector('p');
             let unidades = parseInt(unidadesElemento.innerText, 10);
 
-            // Acceder al elemento de precio correspondiente al producto del botón clickeado
+
             let precioElemento = boton.closest('.productos__listado').querySelector('.producto__precio p');
             let precioTexto = precioElemento.innerText.replace(' USD', '');
             let precio = parseInt(precioTexto, 10);
 
-            // Incrementar o decrementar unidades según el botón clickeado
+
             if (boton.textContent === '+') {
                 unidades++;
-                total += precio; // Sumar el precio al total
+                total += precio;
             } else if (boton.textContent === '-' && unidades > 0) {
                 unidades--;
-                total -= precio; // Restar el precio del total
+                total -= precio;
             }
 
-            // Actualizar el texto del elemento de unidades
+
             unidadesElemento.innerText = unidades;
-            // Actualizar el texto del total
             totalElement.innerText = total;
         });
     });
@@ -35,17 +34,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-class Producto{
-    constructor(nombre, precio, stock){
-        this.nombre =nombre,
-        this.precio =precio,
-        this.stock =stock
-    }
-}
-
-
-let bujia = new Producto('Bujia', 45, 20);
-let cilindro = new Producto('Cilindro', 40, 20);
-let piston = new Producto('Piston', 35, 20);
 
