7be29878a6ed616ebe440cb355fc24cec9385ef6
Cristian-229 <rodriguezcristian26@hotmail.com>
2024-06-11 20:26:55 -0300

    se saco el carrito

--- main.js
+++ main.js
@@ -15,70 +15,76 @@ const productos = [
 
 const productosContainer = document.querySelector('.productos');
 
-productos.forEach(producto => {
-    const productoListado = document.createElement('div');
-    productoListado.classList.add('productos__listado');
+function crearProductoElementos(productos) {
+    const productosContainer = document.querySelector('.productos');
+    productosContainer.innerHTML = ''; 
 
-    const productoFoto = document.createElement('div');
-    productoFoto.classList.add('producto__foto');
-    const img = document.createElement('img');
-    img.classList.add('producto__item');
-    img.src = producto.img;
-    productoFoto.appendChild(img);
+    productos.forEach(producto => {
+        const productoListado = document.createElement('div');
+        productoListado.classList.add('productos__listado');
 
-    const productoDescripcion = document.createElement('div');
-    productoDescripcion.classList.add('producto__descripcion');
-    const descripcion = document.createElement('p');
-    descripcion.textContent = `Esto es un(a) ${producto.nombre}`;
-    productoDescripcion.appendChild(descripcion);
+        const productoFoto = document.createElement('div');
+        productoFoto.classList.add('producto__foto');
+        const img = document.createElement('img');
+        img.classList.add('producto__item');
+        img.src = producto.img;
+        productoFoto.appendChild(img);
 
-    const productoPrecio = document.createElement('div');
-    productoPrecio.classList.add('producto__precio');
-    const precio = document.createElement('p');
-    precio.id = `precio__${producto.nombre.toLowerCase()}`;
-    precio.textContent = producto.precio;
-    productoPrecio.appendChild(precio);
+        const productoDescripcion = document.createElement('div');
+        productoDescripcion.classList.add('producto__descripcion');
+        const descripcion = document.createElement('p');
+        descripcion.textContent = `Esto es un(a) ${producto.nombre}`;
+        productoDescripcion.appendChild(descripcion);
 
-    const productoStock = document.createElement('div');
-    productoStock.classList.add('producto__stock');
-    const stock = document.createElement('p');
-    stock.textContent = producto.stock;
-    productoStock.appendChild(stock);
+        const productoPrecio = document.createElement('div');
+        productoPrecio.classList.add('producto__precio');
+        const precio = document.createElement('p');
+        precio.id = `precio__${producto.nombre.toLowerCase()}`;
+        precio.textContent = producto.precio;
+        productoPrecio.appendChild(precio);
 
-    const productoCantidad = document.createElement('div');
-    productoCantidad.classList.add('producto__cantidad');
-    const agregarSacar = document.createElement('div');
-    agregarSacar.classList.add('producto__agregarSacar');
-    const botonSacar = document.createElement('button');
-    botonSacar.classList.add('producto__boton');
-    botonSacar.textContent = '-';
-    const unidades = document.createElement('p');
-    unidades.id = `unidades__${producto.nombre.toLowerCase()}`;
-    unidades.textContent = '0';
-    const botonAgregar = document.createElement('button');
-    botonAgregar.classList.add('producto__boton');
-    botonAgregar.textContent = '+';
-    agregarSacar.appendChild(botonSacar);
-    agregarSacar.appendChild(unidades);
-    agregarSacar.appendChild(botonAgregar);
-    productoCantidad.appendChild(agregarSacar);
+        const productoStock = document.createElement('div');
+        productoStock.classList.add('producto__stock');
+        const stock = document.createElement('p');
+        stock.textContent = producto.stock;
+        productoStock.appendChild(stock);
 
-    const productoCarrito = document.createElement('div');
-    productoCarrito.classList.add('producto__carrito');
-    const carritoTexto = document.createElement('h2');
-    carritoTexto.textContent = 'Agregar';
-    const carritoImg = document.createElement('img');
-    carritoImg.src = '../assets/img/cart.svg';
-    carritoImg.alt = 'cart';
-    productoCarrito.appendChild(carritoTexto);
-    productoCarrito.appendChild(carritoImg);
+        const productoCantidad = document.createElement('div');
+        productoCantidad.classList.add('producto__cantidad');
+        const agregarSacar = document.createElement('div');
+        agregarSacar.classList.add('producto__agregarSacar');
+        const botonSacar = document.createElement('button');
+        botonSacar.classList.add('producto__boton');
+        botonSacar.textContent = '-';
+        const unidades = document.createElement('p');
+        unidades.id = `unidades__${producto.nombre.toLowerCase()}`;
+        unidades.textContent = '0';
+        const botonAgregar = document.createElement('button');
+        botonAgregar.classList.add('producto__boton');
+        botonAgregar.textContent = '+';
+        agregarSacar.appendChild(botonSacar);
+        agregarSacar.appendChild(unidades);
+        agregarSacar.appendChild(botonAgregar);
+        productoCantidad.appendChild(agregarSacar);
 
-    productoListado.appendChild(productoFoto);
-    productoListado.appendChild(productoDescripcion);
-    productoListado.appendChild(productoPrecio);
-    productoListado.appendChild(productoStock);
-    productoListado.appendChild(productoCantidad);
-    productoListado.appendChild(productoCarrito);
+    
 
-    productosContainer.appendChild(productoListado);
+        productoListado.appendChild(productoFoto);
+        productoListado.appendChild(productoDescripcion);
+        productoListado.appendChild(productoPrecio);
+        productoListado.appendChild(productoStock);
+        productoListado.appendChild(productoCantidad);
+  
+
+        productosContainer.appendChild(productoListado);
+    });
+}
+
+crearProductoElementos(productos);
+
+
+document.getElementById('busqueda').addEventListener('input', function() {
+    const textoBusqueda = this.value.toLowerCase();
+    const productosFiltrados = productos.filter(producto => producto.nombre.toLowerCase().includes(textoBusqueda));
+    crearProductoElementos(productosFiltrados);
 });
